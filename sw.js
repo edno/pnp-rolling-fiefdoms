f2e911ca2da999007e244a20acc719434901cc12
Gregory Heitz <324670+edno@users.noreply.github.com>
2025-12-10 17:17:22 +0100

    bump cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* eslint-env serviceworker */
-const CACHE_VERSION = "v1.14";
+const CACHE_VERSION = "v1.15";
 const CACHE_NAME = `rf-cache-${CACHE_VERSION}`;
-const APP_VERSION = "v4";
+const APP_VERSION = "v5";
 const SHEET_VERSION = "v1.6";
 const ASSETS = [
   "/",
